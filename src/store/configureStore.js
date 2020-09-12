b75ac5c705721de3638f8399232e98433442e775
magicahmd <magic.ahmd@gmail.com>
2020-09-12 16:54:17 +0300

    add redux dev tools.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
-import orderReducer from "./reducers/orderReducer";
+import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import orderReducer from './reducers/orderReducer';
 
 export function configureStore() {
-  return createStore(orderReducer);
+	return createStore(orderReducer, devToolsEnhancer());
 }
